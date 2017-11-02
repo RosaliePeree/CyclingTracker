92d351aec0298aa3693eaa49e25cb63e0f32fc83
RosaliePeree <rosalie.peree@etu.univ-lyon1.fr>
2017-11-02 10:37:05 +0100

    Lock the screen orientation in the onCreate() of every activity

--- app/src/main/java/com/example/rosalie/cyclingtracker/About.java
+++ app/src/main/java/com/example/rosalie/cyclingtracker/About.java
@@ -4,12 +4,12 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
