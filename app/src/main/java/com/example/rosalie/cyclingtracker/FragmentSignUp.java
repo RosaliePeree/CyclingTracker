9e1dd958cb8330247d5db0ecac6700a96897dc8c
RosaliePeree <rosalie.peree@etu.univ-lyon1.fr>
2017-11-16 16:22:07 +0100

    Setting up the files for sign in and up

---
+++ app/src/main/java/com/example/rosalie/cyclingtracker/FragmentSignUp.java
@@ -0,0 +1,10 @@
+package com.example.rosalie.cyclingtracker;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Rosalie on 16/11/2017.
+ */
+
+public class FragmentSignUp extends Fragment {
+}
