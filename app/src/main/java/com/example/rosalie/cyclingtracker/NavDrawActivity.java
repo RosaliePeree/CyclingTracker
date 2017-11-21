e8fb006bf523ada5ca1a03b9b60b7d915222662a
SpooyRainbow <infernoxe42@hotmail.fr>
2017-11-21 16:16:19 +0100

    Profile + modification on the database

--- app/src/main/java/com/example/rosalie/cyclingtracker/NavDrawActivity.java
+++ app/src/main/java/com/example/rosalie/cyclingtracker/NavDrawActivity.java
@@ -70,11 +70,31 @@ public class NavDrawActivity extends AppCompatActivity
             }
         });
 
+        myRef.child("rides").addValueEventListener(new ValueEventListener() { //gets all of the Rides and store them in the allRides list
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Ride ride = dataSnapshot.getValue(Ride.class);
+                Log.i(ride.getDate(), " date");
+
+                for(DataSnapshot dsp : dataSnapshot.getChildren()) {
+                    allRides.add((dsp.getValue(Ride.class)));
+                    Log.i(dsp.getValue(Ride.class).getDate(), " date");
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
     }else{
         Toast.makeText(NavDrawActivity.this, "Please reconnect !",
                 Toast.LENGTH_LONG).show();
     }
+
 
 
 
